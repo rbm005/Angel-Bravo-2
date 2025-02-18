6fb290ac432069baea716e936a0519400561533b
rbm005 <124189855+rbm005@users.noreply.github.com>
2025-02-18 19:00:06 +0100

    Update paciente.cpp

--- src/paciente.cpp
+++ src/paciente.cpp
@@ -1,8 +1,36 @@
 #include "paciente.h"
 
-Paciente::Paciente(const std::string& nombre, const std::string& numeroID, const std::string& fechaIngreso)
-    : nombre(nombre), numeroID(numeroID), fechaIngreso(fechaIngreso) {}
+Paciente::Paciente(const std::string& nombre, const std::string& id, const std::string& fechaIngreso)
+    : nombre(nombre), id(id), fechaIngreso(fechaIngreso) {}
 
-void Paciente::registrarHistorial(const std::string& historial) {
+std::string Paciente::getNombre() const {
+    return nombre;
+}
+
+void Paciente::setNombre(const std::string& nombre) {
+    this->nombre = nombre;
+}
+
+std::string Paciente::getId() const {
+    return id;
+}
+
+void Paciente::setId(const std::string& id) {
+    this->id = id;
+}
+
+std::string Paciente::getFechaIngreso() const {
+    return fechaIngreso;
+}
+
+void Paciente::setFechaIngreso(const std::string& fechaIngreso) {
+    this->fechaIngreso = fechaIngreso;
+}
+
+void Paciente::agregarHistorialClinico(const std::string& historial) {
     historialClinico.push_back(historial);
+}
+
+std::vector<std::string> Paciente::getHistorialClinico() const {
+    return historialClinico;
 }
