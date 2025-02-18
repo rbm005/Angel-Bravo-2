555ac07be01f6f7dabbd8615a325c3c812433c64
rbm005 <124189855+rbm005@users.noreply.github.com>
2025-02-18 18:58:50 +0100

    Update Cita.cpp

--- src/Cita.cpp
+++ src/Cita.cpp
@@ -1,4 +1,36 @@
 #include "Cita.h"
 
-Cita::Cita(const std::string& fecha, const std::string& urgencia, const Paciente& paciente, const Medico& medico)
-    : fecha(fecha), urgencia(urgencia), paciente(paciente), medico(medico) {}
+Cita::Cita(const std::string& idPaciente, const std::string& idMedico, const std::string& fecha, bool esUrgente)
+    : idPaciente(idPaciente), idMedico(idMedico), fecha(fecha), esUrgente(esUrgente) {}
+
+std::string Cita::getIdPaciente() const {
+    return idPaciente;
+}
+
+void Cita::setIdPaciente(const std::string& idPaciente) {
+    this->idPaciente = idPaciente;
+}
+
+std::string Cita::getIdMedico() const {
+    return idMedico;
+}
+
+void Cita::setIdMedico(const std::string& idMedico) {
+    this->idMedico = idMedico;
+}
+
+std::string Cita::getFecha() const {
+    return fecha;
+}
+
+void Cita::setFecha(const std::string& fecha) {
+    this->fecha = fecha;
+}
+
+bool Cita::getEsUrgente() const {
+    return esUrgente;
+}
+
+void Cita::setEsUrgente(bool esUrgente) {
+    this->esUrgente = esUrgente;
+}
